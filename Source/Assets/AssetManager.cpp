f63a5342f2bc00338eb9a7c9d7f8ebd251c719de
David Brandt <David.Brandt200@outlook.com>
2024-08-13 19:21:46 +0200

    Added Asset Manager

---
+++ Source/Assets/AssetManager.cpp
@@ -0,0 +1,24 @@
+#include "AssetManager.h"
+#include <unordered_map>
+#include <map>
+#include <functional>
+
+using AssetMap = std::unordered_map<std::string, AssetBase*>;
+using LoadMap = std::unordered_map<AssetType, std::function<AssetBase*>(std::string)>;
+
+AssetMap map;
+LoadMap loadMap;
+
+
+void AssetManager::Init()
+{
+    
+}
+
+AssetBase* AssetManager::GetAsset(std::string name)
+{
+    if (map.find(name) == map.end())
+        return nullptr;
+
+    return map[name];
+}
--- Source/Assets/AssetManager.h
+++ Source/Assets/AssetManager.h
@@ -1,1 +1,61 @@
 #pragma once
+
+#include <string>
+#include "../JSON/json.hpp"
+#include <memory>
+
+enum AssetType
+{
+	None,
+	VModel,
+	Texture,
+	Material,
+	Audio
+};
+
+namespace std {
+
+	inline string to_string(AssetType t) {
+		switch (t)
+		{
+		case AssetType::VModel:
+			return "VModel";
+		case AssetType::Texture:
+			return "Texture";
+		case AssetType::Material:
+			return "Material";
+		case AssetType::Audio:
+			return "Audio";
+		default:
+			return "None";
+		}
+	};
+
+}
+
+class AssetBase {
+public:
+	AssetType type;
+};
+
+class VModel : AssetBase {
+public:
+	VModel() { type == AssetType::VModel; };
+
+	uint32_t ID{};
+	uint32_t DefaultTexture{};
+};
+
+class Material : AssetBase
+{
+public:
+	Material() { type == AssetType::Material; };
+	uint32_t ID{};
+};
+
+class AssetManager {
+public:
+	static void Init();
+
+	static AssetBase* GetAsset(std::string name);
+};
